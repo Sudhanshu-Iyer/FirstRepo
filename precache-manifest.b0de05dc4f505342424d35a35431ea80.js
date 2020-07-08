fca21df4952a4b803c17f96bec270e8f6d19d5e2
Sudhanshu-Iyer <sudhanshuiyer98@gmail.com>
2020-07-09 01:19:52 +0530

    Updates

---
+++ precache-manifest.b0de05dc4f505342424d35a35431ea80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bb60f9a4a48dc271f19708f66143a6",
+    "url": "/FirstRepo/index.html"
+  },
+  {
+    "revision": "18406a51d09644648a44",
+    "url": "/FirstRepo/static/css/main.25f74fb8.chunk.css"
+  },
+  {
+    "revision": "27eaf10a7f83eb93d99b",
+    "url": "/FirstRepo/static/js/2.a3becef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstRepo/static/js/2.a3becef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18406a51d09644648a44",
+    "url": "/FirstRepo/static/js/main.b9b40a34.chunk.js"
+  },
+  {
+    "revision": "9e86b84ffd3294d7793e",
+    "url": "/FirstRepo/static/js/runtime-main.038b2b51.js"
+  }
+]);
